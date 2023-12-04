8dfebafd9535c39b879c8bfd13030189dadd85b6
Elijah Cajucom <elijah.cajucom@gmail.com>
2023-12-04 20:38:30 +0800

    added placeholder content

--- src/pages/pagetab/MyLibrary.jsx
+++ src/pages/pagetab/MyLibrary.jsx
@@ -3,7 +3,44 @@
 const MyLibrary = () => {
   return (
     <div>
-        This is the library page, where all your books are stored biatch
+        <div className=''>
+            <div className='h-[250px] '>
+                <h1 className='text-2xl font-bold underline'>
+                Hello World!!!
+                </h1>
+                
+            </div>
+            <div className='h-[250px] bg-green-100'>
+                <h1 className='text-2xl font-bold underline'>
+                Hello World!!!
+                </h1>
+                
+            </div>
+            <div className='h-[250px] bg-green-100'>
+                <h1 className='text-2xl font-bold underline'>
+                Hello World!!!
+                </h1>
+                
+            </div>
+            <div className='h-[250px] bg-green-100'>
+                <h1 className='text-2xl font-bold underline'>
+                Hello World!!!
+                </h1>
+                
+            </div>
+            <div className='h-[250px] bg-green-100'>
+                <h1 className='text-2xl font-bold underline'>
+                Hello World!!!
+                </h1>
+                
+            </div>
+            <div className='h-[250px] bg-green-100'>
+                <h1 className='text-2xl font-bold underline'>
+                Hello World!!!
+                </h1>
+                
+            </div>
+        </div>
     </div>
   )
 }
