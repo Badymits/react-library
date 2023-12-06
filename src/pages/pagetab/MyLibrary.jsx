2b01ee2bdc0496f2d43fa5e49bd0e3c6aa1458f4
Elijah Cajucom <elijah.cajucom@gmail.com>
2023-12-07 01:14:14 +0800

    added temporary state and effect to store books from googlebooksapi

--- src/pages/pagetab/MyLibrary.jsx
+++ src/pages/pagetab/MyLibrary.jsx
@@ -1,6 +1,22 @@
+import { useState, useEffect } from 'react';
 
+import getBooksFromAPI from '../../axios/getBooksAPI';
 
+// This page contains the books that the user liked/bookmarked and will provide a list of recommendations
 const MyLibrary = () => {
+
+  const [bookAPI, setBookAPI] = useState([])
+
+  useEffect(() => {
+    getBooksFromAPI().then(res => {
+        console.log(res)
+        setBookAPI(res)
+    })
+  }, [])
+
+
+  console.log(bookAPI)
+
   return (
     <div>
         <div className=''>
