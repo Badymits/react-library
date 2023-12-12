71f5e10909c747476a73de2a55fde3d652e61555
Elijah Cajucom <elijah.cajucom@gmail.com>
2023-12-12 22:16:19 +0800

    added detail view component for right column

---
+++ src/components/DetailView.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+
+
+const BookDetailView = ({ title, author, genre, description }) => {
+  return (
+    <div className="w-full h-full flex justify-center">
+      <div className="pt-8">
+        <h1 className="text-4xl">Welcome!</h1>
+      </div>
+        
+    </div>
+  )
+}
+
+export default BookDetailView
