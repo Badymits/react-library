d3b8ebbe97430d8a1a6aed7bc1fcff9d4d4c51aa
Elijah Cajucom <elijah.cajucom@gmail.com>
2023-12-12 22:29:37 +0800

    fixed styling

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -9,11 +9,11 @@ const Library = () => {
 //   const [currentTab, setcurrentTab] = useState('popular')
 
   return (
-    <section className='relative'>
-        <div className=' sticky top-0 w-full  border-b-2 border-gray-400 z-[100]'>
+    <section className='relative border-x-2 overflow-y-hidden'>
+        <div className='border-b-2 border-gray-400 z-[100] fixed w-[49.8%] top-0  h-[80px] mb-[200px]'>
             <PageHeader />
         </div>
-        <div className='px-6 pt-10'>
+        <div className='px-6 pt-[100px]'>
             {/* Nested Routes will render here */}
             <Outlet />
         </div>
