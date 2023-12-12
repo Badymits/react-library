03c1cafe7e6c65bd1074f87d351b75ed488e4afe
Elijah Cajucom <elijah.cajucom@gmail.com>
2023-12-13 01:53:16 +0800

    added loggedin prop and state to change style accordingly

--- src/components/DetailView.jsx
+++ src/components/DetailView.jsx
@@ -1,12 +1,22 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
+import { useEffect, useState } from 'react';
 
 
-const BookDetailView = ({ title, author, genre, description }) => {
+const BookDetailView = ({ title, author, genre, description, loggedIn }) => {
+  
+  const [bookDetail, setBookDetail] = useState(null)
+
+  // useEffect(() => {
+
+  // }, [])
+
   return (
-    <div className="w-full h-full flex justify-center">
-      <div className="pt-8">
-        <h1 className="text-4xl">Welcome!</h1>
+    <div className={loggedIn ? `w-full h-full flex justify-center` : 'hidden'}>
+      <div className={`pt-8`}>
+        <h1 className="text-4xl">
+          Welcome
+        </h1>
       </div>
         
     </div>
