c5bfb12365eba3c13f700f76ccdc66df9bdaf3f9
Elijah Cajucom <elijah.cajucom@gmail.com>
2023-12-14 00:35:26 +0800

    added styling and structure to display selected book and added api call

--- src/pages/BookDetailView.jsx
+++ src/pages/BookDetailView.jsx
@@ -1,31 +1,86 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
 
+import { AuthContext } from '../context/AuthContext';
 
-const BookDetailView = ({ title, book_id, loggedIn, isClicked }) => {
+import { IoMdClose } from "react-icons/io";
+import { IoLogoFacebook, IoLogoTiktok  } from "react-icons/io5";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { SiGmail } from "react-icons/si";
+
+const BookDetailView = ({ title, loggedIn }) => {
   
-  const [bookDetail, setBookDetail] = useState(null)
-  console.log('here')
-  let { bookId } = useParams();
-  console.log(bookId)
-  // useEffect(() => {
+  // when using basic conditional rendering, just leave the state empty, dont put any values
+  const [bookDetailComp, setBookDetailComp] = useState()
 
-  // }, [])
+  const closeRightPanel = () => {
+    console.log('tangina mag close ka na')
+    setBookDetailComp()
+  }
 
-  console.log('Clicked on: ', book_id)
+  let { bookId } = useContext(AuthContext)
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+      const data = await axios.get( `http://127.0.0.1:8000/get-book-detail/${bookId}/`)
+      return data
+    }
+    fetchData().then((res) => {
+      console.log(res.data)
+      setBookDetailComp(res.data)
+    }).catch((err) => (
+      console.log(err)
+    ))
+    
+  }, [bookId])
+
 
   return (
-    <div className={loggedIn ? `w-full h-full flex justify-center` : 'hidden'}>
-      <div className={`pt-8`}>
-        <h1>aaa</h1>
-        <h1>{bookId}</h1>
-        <h1 className="text-4xl">
-          Welcome
-        </h1>
-      </div>
-        
+    <div className={loggedIn ? 'w-full h-full flex justify-center relative' : 'hidden'}>
+      <div>
+            {
+              bookDetailComp ?
+              
+              <div className='w-full h-screen bg-gray-200 flex flex-col items-center pt-[40px]  px-8 text-justify align-center gap-6'>
+                <IoMdClose className='text-2xl absolute top-10 right-5 hover:bg-gray-200 rounded-full cursor-pointer' onClick={() => closeRightPanel()}/>
+                
+                <img src={bookDetailComp.book_image} alt={bookDetailComp.title} className='max-h-[350px] max-w-[350px]' />
+                <p>{bookDetailComp.title}</p>
+                <p>{bookDetailComp.author.name}</p>
+                <p className=''>{bookDetailComp.summary}</p>
+                <button className='absolute bottom-10 bg-[#6AB187] w-[80%] py-3 rounded-full mt-10 text-white text-2xl'>
+                  Add to Cart
+                </button>
+              </div>
+              :
+              <div className='w-full h-screen pt-[20px] px-6 text-justify '>
+                <h1 className='text-center text-4xl'>Welcome!!</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam sit nemo magni vel, 
+                  dolorum dicta earum. Veniam adipisci eaque rerum excepturi possimus ipsum voluptatem. 
+                  Numquam sint voluptates voluptate temporibus aspernatur?</p>
+                <p>more lorem ipsum whit</p>
+                <p>more lorem ipsum whit</p>
+                <br />
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam sit nemo magni vel, 
+                  dolorum dicta earum. Veniam adipisci eaque rerum excepturi possimus ipsum voluptatem. 
+                  Numquam sint voluptates voluptate temporibus aspernatur?</p>
+                  <br />
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam sit nemo magni vel, 
+                  dolorum dicta earum. Veniam adipisci eaque rerum excepturi possimus ipsum voluptatem. 
+                  Numquam sint voluptates voluptate temporibus aspernatur?</p>
+                <div className='flex items-center gap-4 absolute bottom-10 right-5 left-10 text-[#38a164]'>
+                  <h1 className=''>You may contact and <br/> follow us on:</h1>
+                  <IoLogoFacebook className='cursor-pointer text-4xl hover:text-[#6AB187] duration-200'/>
+                  <FaSquareXTwitter className='cursor-pointer text-4xl hover:text-[#6AB187] duration-200'/>
+                  <IoLogoTiktok className='cursor-pointer text-4xl hover:text-[#6AB187] duration-200'/>
+                  <SiGmail className='cursor-pointer text-4xl hover:text-[#6AB187] duration-200'/>
+                </div>
+              </div>
+            }
+          </div>  
     </div>
   )
 }
