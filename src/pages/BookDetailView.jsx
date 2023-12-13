ea4e9f8e084316739403992b10ef20b58c9e8d7c
Elijah Cajucom <elijah.cajucom@gmail.com>
2023-12-13 18:24:26 +0800

    updated name

--- src/pages/BookDetailView.jsx
+++ src/pages/BookDetailView.jsx
@@ -1,19 +1,26 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 
-const BookDetailView = ({ title, author, genre, description, loggedIn }) => {
+const BookDetailView = ({ title, book_id, loggedIn, isClicked }) => {
   
   const [bookDetail, setBookDetail] = useState(null)
-
+  console.log('here')
+  let { bookId } = useParams();
+  console.log(bookId)
   // useEffect(() => {
 
   // }, [])
 
+  console.log('Clicked on: ', book_id)
+
   return (
     <div className={loggedIn ? `w-full h-full flex justify-center` : 'hidden'}>
       <div className={`pt-8`}>
+        <h1>aaa</h1>
+        <h1>{bookId}</h1>
         <h1 className="text-4xl">
           Welcome
         </h1>
