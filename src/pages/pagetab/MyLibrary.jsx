69d38c85f6f5a41f08a2a8d01d72c84e5605be11
Elijah Cajucom <elijah.cajucom@gmail.com>
2023-12-14 00:37:07 +0800

    added placeholder data

--- src/pages/pagetab/MyLibrary.jsx
+++ src/pages/pagetab/MyLibrary.jsx
@@ -1,62 +1,31 @@
-import { useState, useEffect } from 'react';
+import { useContext } from "react"
+import { AuthContext } from "../../context/AuthContext"
+import SwiperComponent from "../../components/SwiperComponent"
 
-import getBooksFromAPI from '../../axios/getBooksAPI';
 
 // This page contains the books that the user liked/bookmarked and will provide a list of recommendations
 const MyLibrary = () => {
 
-  const [bookAPI, setBookAPI] = useState([])
+    let { books } = useContext(AuthContext)
 
-  useEffect(() => {
-    getBooksFromAPI().then(res => {
-        console.log(res)
-        setBookAPI(res)
-    })
-  }, [])
-
-
-  console.log(bookAPI)
+    // let this variable contain the search parameters (to be implemented and optimized later)
+    const bookGenre = ['Computer Science', 'Fiction']
+    const bookGenreFic = [ 'Fiction']
+    
+    // filtering nested arrays since books can have multiple genres
+    // will be modified later so it can filter by multiple queries (title, author, and genre)
+    const resultCom = books.filter(book => book.genre.every(b => bookGenre.includes(b.name)))
+    const resultFic = books.filter(book => book.genre.every(b => bookGenreFic.includes(b.name)))
 
   return (
     <div>
-        <div className=''>
-            <div className='h-[250px] '>
-                <h1 className='text-2xl font-bold underline'>
-                Hello World!!!
-                </h1>
-                
-            </div>
-            <div className='h-[250px] bg-green-100'>
-                <h1 className='text-2xl font-bold underline'>
-                Hello World!!!
-                </h1>
-                
-            </div>
-            <div className='h-[250px] bg-green-100'>
-                <h1 className='text-2xl font-bold underline'>
-                Hello World!!!
-                </h1>
-                
-            </div>
-            <div className='h-[250px] bg-green-100'>
-                <h1 className='text-2xl font-bold underline'>
-                Hello World!!!
-                </h1>
-                
-            </div>
-            <div className='h-[250px] bg-green-100'>
-                <h1 className='text-2xl font-bold underline'>
-                Hello World!!!
-                </h1>
-                
-            </div>
-            <div className='h-[250px] bg-green-100'>
-                <h1 className='text-2xl font-bold underline'>
-                Hello World!!!
-                </h1>
-                
-            </div>
-        </div>
+        <div className='relative pt-10 mb-[10em]'>
+        <SwiperComponent 
+          resultComSci={resultCom}
+          genre={'ComputerScience'}
+          title='Computer Science'
+        />  
+      </div>
     </div>
   )
 }
