52e83606d177779adb9686eff2fcce103339ffca
Elijah Cajucom <elijah.cajucom@gmail.com>
2023-12-14 01:10:51 +0800

    added state and function to handle searchbar input

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -1,11 +1,20 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { NavLink } from "react-router-dom"
 import { CiSearch } from "react-icons/ci";
 import { GiShoppingCart } from "react-icons/gi";
+import { AuthContext } from "../context/AuthContext";
 
 const PageHeader = () => {
 
+  let { handleSearch } = useContext(AuthContext)
+ 
   const [navbar, setNavbar] = useState(false)
+  const [searchBar, setsearchBar] = useState('')
+
+  const handleSearchBar = (e) => {
+    console.log(e.target.value)
+    setsearchBar(e.target.value)
+  }
 
   const handleNavbar = () => {
 
@@ -17,7 +26,7 @@ const PageHeader = () => {
   }
 
   window.addEventListener('scroll', handleNavbar)
-
+ 
   return (
     <header className={navbar ? "h-[80px] px-6 bg-white/10 backdrop-blur-md border-gray-400  transition-all" : "px-6 w-full h-[80px] " }>
         <div className="flex items-center justify-between h-full">
@@ -69,7 +78,18 @@ const PageHeader = () => {
             <div className="flex w-[50%] items-center justify-between mx-2 ">
                 <div className="relative">
                     <CiSearch className="absolute top-1 right-1 text-lg"/>
-                    <input type="text" placeholder="Search for title, author, genre..." className="w-[350px] border border-slate-500 rounded-lg" />
+                    <form action="" onSubmit={handleSearch}>
+                        <input 
+                            type="text" 
+                            name="searchbar"
+                            placeholder="Search for title, author, genre..." 
+                            className="w-[350px] border border-slate-500 rounded-lg" 
+                            value={searchBar}
+                            onChange={handleSearchBar}
+                            onSubmit={handleSearch}
+                        />
+                    </form>
+                    
                 </div>
                 <GiShoppingCart className="text-3xl hover:hover:bg-gray-200 rounded-full cursor-pointer"/>
             </div>
