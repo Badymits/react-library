c6d30f7cc18fdf69b64c87a17644542f0b251939
Elijah Cajucom <elijah.cajucom@gmail.com>
2023-12-18 18:45:45 +0800

    added useeffect and storage to store books and persist on page refresh

--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -1,20 +1,31 @@
-import { useContext } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import { AuthContext } from '../context/AuthContext'
 
 const SearchResult = () => {
-  
-    let { keyword, searchResults } = useContext(AuthContext)
+
+  let { searchResults } = useContext(AuthContext)
+  const keyword = sessionStorage.getItem('keyword')
+
+  const [books, setBooks] = useState(() => [])
+
+  //when search results changes, set the books state to the items in the session storage
+  useEffect(() => {
+    setBooks(JSON.parse(sessionStorage.getItem('books')))
+  }, [searchResults])
 
   return (
-     
-        <div className='h-[100vh]'>
+        <div>
           <p className='font-bold'>Search Results for: {keyword}</p>
-          {/* display all books related to keyword here... */}
-          {searchResults.map((book) => (
-            <div key={book.id}>
-              <p>{book.title}</p>
-            </div>
-          ))}
+          <div>
+            {books.map((book) => (
+              <div key={book.id} className='h-[200px]'>
+                <img src={book.book_image} alt="" className='h-[100px] w-[100px]'/>
+                <p>{book.title}</p>
+                <p>{book.author.name}</p>
+                <p>{book.genre[0].name}</p>
+              </div>
+            ))}
+          </div>
         </div>
   )
 }
