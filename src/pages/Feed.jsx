545bd99bbf6f843b9d66e3303624bad51af4cbfd
Elijah Cajucom <elijah.cajucom@gmail.com>
2023-12-18 18:39:50 +0800

    updated styling

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,12 +1,16 @@
+import PageHeader from "../components/PageHeader"
 
 
 const Feed = () => {
   return (
-    <section className='border-x-2'>
-        <div>
+    <section className='xl:max-w-[930px] lg:max-w-[760px]'>
+        <div className='border-b-2 border-gray-400 z-[100] sticky w-full top-0  h-[80px] '>
+            <PageHeader />
+        </div>
+        <div className="">
             Feed page biatch
         </div>
-        <div className='h-[250px] bg-green-100'>
+        <div className='h-[250px] bg-green-100 xl:max-w-[930px]'>
             <h1 className='text-2xl font-bold underline'>
             Hello World!!!
             </h1>
