c02e4c39b25d9c6271f848680c6bb8a2ad696967
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-01-07 21:29:51 +0800

    created context and added state to preserve multi-step form data

--- src/pages/pagetab/CheckOut.jsx
+++ src/pages/pagetab/CheckOut.jsx
@@ -1,13 +1,39 @@
-
+import { createContext, useEffect, useState } from "react"
 import FormComponent from "../../components/FormComponent"
-//import { IoIosRemoveCircleOutline, IoMdAddCircleOutline } from "react-icons/io"
+
+
+export const CheckoutContext = createContext()
+
+// =====================================================
+// READ IMPORTANT
+// this component will be responsible for communicating with the backend and 
+// preservation of data throughout the multistep form
+// =====================================================
 
 const CheckOut = () => {
 
+  const testVar = 'It is working'
+
+  const [checkOutFormData, setCheckOutFormData] = useState(null)
+
+  // different from booksInCart array state since this will be utilized by a different model in the backend, 
+  // that includes the rent or purchase status
+  const [checkOutBooks, setCheckOutBooks] = useState([])
+
+  console.log(checkOutBooks)
+
+  // const stripe = Stripe('pk_test_51LHqMdDenqsfOr1HJvOFflHXQQExeSgmjXHiIY6QJURHpRzT6XtFU95MZgaaL6qj3NCWi6Gnqrgcwgv9YS1gQY7Q00IPUQ0Jch')
+
+  let CheckoutData = {
+    testVar: testVar,
+    checkOutBooks: checkOutBooks,
+    setCheckOutBooks: setCheckOutBooks,
+  }
+
   return (
-    <div className="">
+    <CheckoutContext.Provider value={CheckoutData}>
       <FormComponent />
-    </div>
+    </CheckoutContext.Provider>
   )
 }
 
