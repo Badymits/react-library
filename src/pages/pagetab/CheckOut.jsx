06a2865be903be013fae9cf4820020f5747e2c69
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-01-10 23:00:20 +0800

    added state and useeffect to retrieve client secret

--- src/pages/pagetab/CheckOut.jsx
+++ src/pages/pagetab/CheckOut.jsx
@@ -1,7 +1,15 @@
 import { createContext, useEffect, useState } from "react"
 import FormComponent from "../../components/FormComponent"
 
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import axios from "axios";
 
+
+
+// eslint-disable-next-line no-undef
+const stripePromise = loadStripe(`${import.meta.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}`)
+console.log(stripePromise)
 export const CheckoutContext = createContext()
 
 // =====================================================
@@ -11,10 +19,18 @@ export const CheckoutContext = createContext()
 // =====================================================
 
 const CheckOut = () => {
-
   const testVar = 'It is working'
 
   const [checkOutFormData, setCheckOutFormData] = useState(null)
+
+  const [amount, setAmount] = useState(0)
+
+
+  const [clientID, setClientID] = useState('')
+  const [clientSecretOne, setClientSecretOne] = useState('')
+
+  // if set to true, will create a payment intent in the backend
+  const [paymentIntent, setPaymentIntent] = useState(() => true)
 
   // different from booksInCart array state since this will be utilized by a different model in the backend, 
   // that includes the rent or purchase status
@@ -24,19 +40,50 @@ const CheckOut = () => {
 
   console.log(checkOutBooks)
 
-  // const stripe = Stripe('pk_test_51LHqMdDenqsfOr1HJvOFflHXQQExeSgmjXHiIY6QJURHpRzT6XtFU95MZgaaL6qj3NCWi6Gnqrgcwgv9YS1gQY7Q00IPUQ0Jch')
+  useEffect(() => {
+    const createPaymentIntent = async () => {
+      const data = await axios.get('http://127.0.0.1:8000/payments/create-payment-intent/')
+      return data
+    }
+     
+    if (paymentIntent === true){
+      createPaymentIntent().then((res) => {
+        console.log(res.data)
+        setClientID(res.data.client_secret.id)
+        setClientSecretOne(res.data.client_secret.client_secret)
+      })
+    }
+    
+  }, [paymentIntent, setClientID, setClientSecretOne])
 
+
+  console.log('potangina ', clientID)
+  console.log('potanginatalaga ',clientSecretOne)
+  
   let CheckoutData = {
     testVar: testVar,
     checkOutBooks: checkOutBooks,
     setCheckOutBooks: setCheckOutBooks,
     checkOutFormData: checkOutFormData,
     setCheckOutFormData: setCheckOutFormData,
+    stripePromise: stripePromise,
+    setPaymentIntent: setPaymentIntent,
+    amount: amount,
+    setAmount: setAmount,
+    clientSecretOne: clientSecretOne,
+    
   }
 
   return (
     <CheckoutContext.Provider value={CheckoutData}>
-      <FormComponent />
+      
+      <Elements
+        options={clientSecretOne}
+        stripe={stripePromise}
+      >
+        <FormComponent />
+      </Elements>
+      
     </CheckoutContext.Provider>
   )
 }
